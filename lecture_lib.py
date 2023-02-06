0def243e3c823d9819385e5d711b7d4e2c098f95
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2023-02-06 16:05:46 +0900

    csv

--- lecture_lib.py
+++ lecture_lib.py
@@ -1,5 +1,6 @@
 _DB_FILE = "sales.sqlite"
 
+
 def download_db(db_file=_DB_FILE):
     import os
     if os.path.exists(_DB_FILE):
@@ -9,10 +10,29 @@ def download_db(db_file=_DB_FILE):
     url = f"https://drive.google.com/uc?id={id}"
     gdown.download(url, db_file, quiet=False)
 
-download_db(_DB_FILE)
 
 def query(sql):
     import pandas as pd
     import sqlite3
     con = sqlite3.connect(_DB_FILE)
-    return pd.read_sql_query(sql, con)  
+    return pd.read_sql_query(sql, con)
+
+
+def download_csv():
+    import sqlite3
+    import pandas as pd
+    con = sqlite3.connect(_DB_FILE)
+    sales = pd.read_sql_query("""
+    select * from sales
+    """, con)
+    sales.to_csv("sales.csv")
+
+    stores = pd.read_sql_query("""
+    select * from stores
+    """, con)
+    stores.to_csv("stores.csv")
+
+    features = pd.read_sql_query("""
+    select * from features
+    """, con)
+    features.to_csv("features.csv")
